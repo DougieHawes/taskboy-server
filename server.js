31150465684b61538f4c4e4bba758ba2bb2de3d4
dougiehawes <dougiehawes@hotmail.com>
2025-02-07 11:59:23 +0000

    first commit

---
+++ server.js
@@ -0,0 +1,14 @@
+import cors from "cors";
+import dotenv from "dotenv";
+import express from "express";
+
+dotenv.config();
+
+const app = express();
+
+const PORT = process.env.PORT;
+
+app.use(cors());
+app.use(express.json());
+
+app.listen(PORT, console.log(`express app running on port:${PORT}`));
